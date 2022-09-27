9b72aeb413e30a714681f2f6b415b581dc0c78c2
saff-it <saff_it@ymail.com>
2022-09-27 16:42:48 +0200

    index and show updated in controller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -14,7 +15,15 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return response()->json([
+            'response' => true,
+            'count' => count($posts),
+            'results' => [
+                "data" => $posts,
+            ]    
+            
+        ]);
     }
 
     /**
@@ -46,7 +55,19 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+
+        if ($post) {
+            return response()->json([
+                'response' => true,
+
+                'results' => [
+                    "data" => $post,
+                ]    
+            
+            ]);
+        } else return response('', '404');
+       
     }
 
     /**
